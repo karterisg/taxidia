f4c25590b8f3bf9390d7fa66cba2294e17cd9f26
karterisg <sdi2100258@di.uoa.gr>
2025-11-22 01:55:27 +0200

    new

--- tours.sql
+++ tours.sql
@@ -1,5 +1,5 @@
-#Καρτερης Γιωργος
-#Κολοκουρας Αποστολος
+Καρτερης Γιωργος
+Κολοκουρας Αποστολος
 
 
 #1
@@ -13,15 +13,18 @@ AND d.destination_id = ta.destination_destination_id
 AND d.country = "GERMANY"
 ORDER BY e.name;
 
+
+------------------------------------------------
+
 #2
-SELECT distinct travel_guide_employee_AM  AS travel_guide_id, COUNT(*) AS number_of_guided_tours
+SELECT distinct travel_guide_employee_AM  AS travel_guide_id, count(*) AS number_of_guided_tours
 FROM guided_tour gt , trip_package tp
 WHERE tp.trip_package_id = gt.trip_package_id
 AND (tp.trip_start >= '2019-01-01' AND tp.trip_end < '2020-01-01')
 GROUP BY travel_guide_employee_AM
-HAVING COUNT(*) > 3;
+HAVING Count(*) > 3;
 
-
+--------------------------------------------------
 #3
 SELECT distinct e.travel_agency_branch_travel_agency_branch_id AS travel_agency_brunch_id, COUNT(*) AS number_of_travel_guides
 FROM employees e
@@ -42,6 +45,8 @@ AND d.destination_id = p.destination_destination_id
 GROUP BY p.trip_package_trip_package_id
 ORDER BY trip_package_trip_package_id;
 
+
+----------------------------------------------------
 #5
 SELECT e.name , e.surname
 FROM guided_tour gt, employees e
@@ -49,11 +54,13 @@ WHERE gt.travel_guide_employee_AM = e.employees_AM
 GROUP BY gt.travel_guide_employee_AM
 HAVING COUNT(DISTINCT gt.travel_guide_language_id) = 1;
 
+
+----------------------------------------------------
 #6
 SELECT 'yes' AS answer
 FROM offer o
 WHERE NOT EXISTS (
-    SELECT 1
+    SELECT r.offer_id
     FROM reservation r
     WHERE r.offer_id = o.offer_id
 )
@@ -61,10 +68,12 @@ UNION
 SELECT 'no' AS solution
 FROM offer o
 WHERE EXISTS (
-    SELECT 1
+    SELECT r.offer_id
     FROM reservation r
     WHERE r.offer_id = o.offer_id
 );
+
+----------------------------------------------------
 
 #7
 SELECT t.name , t.surname
@@ -72,9 +81,9 @@ FROM traveler t
 WHERE t.gender = 'male' 
 AND t.age > 2040
 AND t.traveler_id IN (SELECT Customer_id 
-                    FROM reservation
-                    GROUP BY Customer_id
-                    HAVING COUNT(DISTINCT offer_trip_package_id) > 3);
+					FROM reservation
+					GROUP BY Customer_id
+					HAVING COUNT(DISTINCT offer_trip_package_id) > 3);
 
 #8
 SELECT e.employees_AM AS travel_guide_id, e.name, e.surname, COUNT(ta.tourist_attraction_id) AS num_attractions
@@ -86,15 +95,21 @@ AND t.travel_guide_employee_AM = gt.travel_guide_employee_AM
 AND t.languages_id = gt.travel_guide_language_id
 GROUP BY e.employees_AM, e.name, e.surname;
 
+
+----------------------------------------------------
+
 #9
 SELECT d.country
 FROM trip_package_has_destination tpd , destination d
 WHERE tpd.destination_destination_id = d.destination_id
 GROUP BY destination_destination_id
 HAVING COUNT(*) >= ALL (
-    SELECT COUNT(*)
-    FROM trip_package_has_destination
-    GROUP BY destination_destination_id);
+	SELECT COUNT(*)
+	FROM trip_package_has_destination
+	GROUP BY destination_destination_id);
+
+
+----------------------------------------------------
 
 #10
 SELECT tp.trip_package_id
@@ -104,6 +119,8 @@ AND thd.destination_destination_id = d.destination_id
 AND d.country = 'Ireland'
 GROUP BY tp.trip_package_id
 HAVING COUNT(*) = (
-    SELECT COUNT(*)
-    FROM destination d
-    WHERE d.country = 'Ireland');
+	SELECT COUNT(*)
+	FROM destination d
+	WHERE d.country = 'Ireland');
+
+----------------------------------------------------
